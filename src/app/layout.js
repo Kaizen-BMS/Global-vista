be901497d466f9ac1bc96e200d71c448ade1e0e3
anandpandeylive12 <anandpandeylive12@gmail.com>
2026-07-13 19:48:27 +0530

    first commit

--- src/app/layout.js
+++ src/app/layout.js
@@ -47,28 +47,66 @@ export default function RootLayout({ children }) {
       lang="en"
       className={`${inter.variable} ${instrument.variable}`}
     >
-      <head>
-        {/* Google Analytics */}
-        <Script
-          src="https://www.googletagmanager.com/gtag/js?id=G-T71M3NLZCH"
-          strategy="afterInteractive"
-        />
+    <head>
+  {/* Google Analytics */}
+  <Script
+    src="https://www.googletagmanager.com/gtag/js?id=G-T71M3NLZCH"
+    strategy="afterInteractive"
+  />
 
-        <Script id="google-analytics" strategy="afterInteractive">
-          {`
-            window.dataLayer = window.dataLayer || [];
+  <Script id="google-analytics" strategy="afterInteractive">
+    {`
+      window.dataLayer = window.dataLayer || [];
 
-            function gtag() {
-              dataLayer.push(arguments);
-            }
+      function gtag() {
+        dataLayer.push(arguments);
+      }
 
-            gtag('js', new Date());
-            gtag('config', 'G-T71M3NLZCH');
-          `}
-        </Script>
-      </head>
+      gtag('js', new Date());
+      gtag('config', 'G-T71M3NLZCH');
+    `}
+  </Script>
+
+  {/* Meta Pixel */}
+  <Script id="meta-pixel" strategy="afterInteractive">
+    {`
+      !function(f,b,e,v,n,t,s)
+      {
+        if(f.fbq)return;
+        n=f.fbq=function(){
+          n.callMethod ?
+          n.callMethod.apply(n,arguments) : n.queue.push(arguments)
+        };
+        if(!f._fbq)f._fbq=n;
+        n.push=n;
+        n.loaded=!0;
+        n.version='2.0';
+        n.queue=[];
+        t=b.createElement(e);
+        t.async=!0;
+        t.src=v;
+        s=b.getElementsByTagName(e)[0];
+        s.parentNode.insertBefore(t,s);
+      }(window, document,'script',
+      'https://connect.facebook.net/en_US/fbevents.js');
+
+      fbq('init', '3941002349542415');
+      fbq('track', 'PageView');
+    `}
+  </Script>
+</head>
 
       <body className="relative min-h-screen font-body antialiased">
+
+  <noscript>
+    <img
+      height="1"
+      width="1"
+      style={{ display: "none" }}
+      src="https://www.facebook.com/tr?id=3941002349542415&ev=PageView&noscript=1"
+      alt=""
+    />
+  </noscript>
         <GalaxyBackground />
 
         <ScrollProgress />
