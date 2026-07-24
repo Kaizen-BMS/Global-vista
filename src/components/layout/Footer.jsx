a39f888756c72783c14b94301f88fe3d15101718
ANAND PANDEY <anandpandeylive12@gmail.com>
2026-07-24 18:55:37 +0530

    Fix phone number link in Footer component

--- src/components/layout/Footer.jsx
+++ src/components/layout/Footer.jsx
@@ -108,7 +108,7 @@ export default function Footer() {
   </a>
 
   <a
-    href="tel:+91998145 61099"
+    href="tel:+9198145 61099"
     className="flex h-10 w-10 items-center justify-center rounded-full border border-white/10 bg-white/5 text-muted transition-all hover:border-gold/40 hover:text-gold"
   >
     <Phone className="h-4 w-4" />
